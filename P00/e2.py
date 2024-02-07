f5ff672007938d6f8a952a1bf9e74fc12fea1aee
nayaraoa <n.ortiz.2023@alumnos.urjc.es>
2024-02-07 18:40:49 +0100

    :

--- P00/e2.py
+++ P00/e2.py
@@ -2,4 +2,4 @@ from Seq0 import *
 
 filename = 'U5(2).txt'
 print('DNA file: U5.txt')
-print('The first 20 bases are:', '\n', seq_read_fasta(filename)[:20])
+print('The first 20 bases are:', '\n' + seq_read_fasta(filename)[:20])
