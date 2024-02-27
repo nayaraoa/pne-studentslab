9c3a07a7cfdb58253f8fd66f17ce87f128433445
nayaraoa <n.ortiz.2023@alumnos.urjc.es>
2024-02-27 21:16:45 +0100

    .

--- S08/e3.py
+++ S08/e3.py
@@ -3,14 +3,28 @@ import socket
 # SERVER IP, PORT
 PORT = 8081
 IP = "212.128.255.64" # depends on the computer the server is running
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-while True:
-  # -- Ask the user for the message
+try:
+    serversocket.bind((IP, PORT))
+    serversocket.listen()
+    while True:
+        msg = input('Enter the message you want to send:')
 
-  # -- Create the socket
+      # -- Establish the connection to the Server
+        print("Waiting for connections at {}, {} ".format(IP, PORT))
+        (clientsocket, address) = serversocket.accept()
 
-  # -- Establish the connection to the Server
+      # -- Send the user message
+        send_bytes = str.encode(msg)
 
-  # -- Send the user message
+      # -- Close the socket
+        clientsocket.send(send_bytes)
+        clientsocket.close()
 
-  # -- Close the socket
+except socket.error:
+    print("Problems using ip {} port {}. Is the IP correct? Do you have port permission?".format(IP, PORT))
+
+except KeyboardInterrupt:
+    print("Server stopped by the user")
+    serversocket.close()
