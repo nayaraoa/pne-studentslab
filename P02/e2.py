9067971408369d018824f2dcad89e78cb2bc5b86
nayaraoa <n.ortiz.2023@alumnos.urjc.es>
2024-02-28 17:47:49 +0100

    :

---
+++ P02/e2.py
@@ -0,0 +1,15 @@
+from Client0 import Client
+
+PRACTICE = 2
+EXERCISE = 2
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+# -- Parameters of the server to talk to
+IP = "212.128.255.80" # your IP address
+PORT = 8081
+
+c = Client(IP, PORT)
+print(c)
+
+
---
+++ P02/e3.py
@@ -0,0 +1,19 @@
+from Client0 import Client
+
+PRACTICE = 2
+EXERCISE = 2
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+# -- Parameters of the server to talk to
+IP = "212.128.255.64" # your IP address
+PORT = 8081
+
+c = Client(IP, PORT)
+print(c)
+
+# -- Send a message to the server
+print("Sending a message to the server...")
+response = c.talk("Testing!!!")
+print(f"Response: {response}")
+
